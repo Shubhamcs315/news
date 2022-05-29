061887e414f039d62ce7167b06e5cabdd1cdfae0
Shubhamcs315 <sheteshubham315@gmail.com>
2022-05-28 22:32:10 -0700

    initial commit

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -8,7 +8,7 @@ export class Newsitem extends Component {
         <div className="card" style={{ width: "18rem" }}>
           <img src={image} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title} <span class="badge bg-secondary">{source}</span></h5>
+            <h5 className="card-title">{title} <span className="badge bg-secondary">{source}</span></h5>
             <p className="card-text">{description}...</p>
             <p className="card-text"><small className="text-muted">By {this.props.author?this.props.author:"unknown"} on {new Date(this.props.date).toGMTString()}</small></p>
             <a rel="noreferrer"  href={newsId} target="_blank" className="btn btn-sm btn-dark">
